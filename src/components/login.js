bb027323109039a0abcc0ad3077d61113e9a07f5
John D <jddhalogamer@gmail.com>
2022-03-11 17:32:07 -0600

    wrote AddReview component.

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const Login = () => {
+const Login = (props) => {
 
   const [ name, setName ] = useState("")
   const [ id, setId ] = useState("")
