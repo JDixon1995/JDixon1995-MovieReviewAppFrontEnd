fbb77c89262c4e083d3a01340fc641f4c17fb199
John D <jddhalogamer@gmail.com>
2022-03-10 20:21:38 -0600

    added state hooks to Login component.

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Login = () => {
-  return <div>
-      Login
-  </div>;
+
+  const [ name, setName ] = useState("")
+  const [ id, setId ] = useState("")
+
+  const onChangeName = e => {
+    const name = e.target.value
+    setName(name)
+  }
+
+  const onChangeId = e => {
+    const id = e.target.value
+    setId(id)
+  }
+
+  const login = () => {
+    props.login({name: name, id: id})
+    props.history.push('/')   
+  }
+
+    return(
+      <div>
+        
+      </div>
+    )
 };
 
 export default Login;
