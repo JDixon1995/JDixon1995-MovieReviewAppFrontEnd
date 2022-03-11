02b1617f4e8a88339c5c8ae4c03a326cbf41eed1
John D <jddhalogamer@gmail.com>
2022-03-10 20:31:49 -0600

    added Login component.

--- src/components/login.js
+++ src/components/login.js
@@ -24,7 +24,27 @@ const Login = () => {
 
     return(
       <div>
-        
+        <Form>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+            type='text'
+            placeholder='Enter username'
+            value={name}
+            onChange={onChangeName}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>ID</Form.Label>
+            <Form.Control
+            type='text'
+            placeholder='Enter id'
+            value={id}
+            onChange={onChangeId}
+            ></Form.Control>
+          </Form.Group>
+          <Button variant='primary' onClick={login}>Submit</Button>
+        </Form>
       </div>
     )
 };
